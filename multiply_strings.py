6a5f87010d75cd1fc63385e8fbf9d2dcae492298
Jonathan Hatekah <102974087+underagee@users.noreply.github.com>
2023-06-17 21:08:19 +0000

    Multiply string

---
+++ multiply_strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        result = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                mul = int(num1[i]) * int(num2[j])
+                result[i + j] += mul
+                result[i + j + 1] += result[i + j] // 10
+                result[i + j] %= 10
+
+        result = result[::-1] 
+        result_str = "".join(map(str, result)).lstrip('0')
+        return result_str
+
